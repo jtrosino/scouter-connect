cc69cc1cc8cf3ff5728467ac298a870ebd118ce0
James <jtrosino@outlook.com>
2025-07-15 13:38:31 -0300

    Fix port to use Render environment variable

--- server.js
+++ server.js
@@ -492,7 +492,7 @@ app.get('*', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
