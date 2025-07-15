28b2a02a445ca2bc089d1658fba9aa0efa125b4b
James <jtrosino@outlook.com>
2025-07-15 13:50:13 -0300

    Ensure correct port setup

--- server.js
+++ server.js
@@ -492,7 +492,7 @@ app.get('*', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
